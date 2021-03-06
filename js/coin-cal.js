d5b6aca0055a61fe821e976f83643bff9c66c503
kh-aos <sunghoonkritesh49@gmail.com>
2021-03-06 15:24:50 +0400

    added things

--- js/coin-cal.js
+++ js/coin-cal.js
@@ -83,11 +83,28 @@ async function returnWorth2(){
     var currentPrice = obj2["market_data"]["current_price"]["usd"];
         
     document.getElementById("displayNetWortInvestedhAmount").value = new Number((currentPrice / previousPrice) * investedAmount);
-    
-
-
     }
-
 }
 // https://api.coingecko.com/api/v3/coins/
 //https://www.coingecko.com/api/documentations/v3#/simple/get_simple_token_price__id_
+
+var slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}
